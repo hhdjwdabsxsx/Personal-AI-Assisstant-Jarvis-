2ec95c1ccd63567fc70c6623cc749050176fd30f
Yash Raj Sinha <121656209+hhdjwdabsxsx@users.noreply.github.com>
2025-01-06 10:28:52 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,81 @@
+import requests
+import wikipedia
+import pywhatkit as kit
+from email.message import EmailMessage
+import smtplib
+import gtts
+import os
+from decouple import config
+from pydub import AudioSegment
+from pydub.playback import play
+from constants import (
+    EMAIL,
+    PASSWORD,
+    IP_ADDR_API_URL,
+    NEWS_FETCH_API_URL,
+    SMTP_URL,
+    SMTP_PORT,
+    NEWS_FETCH_API_KEY,
+)
+
+# Specify the path to the FFmpeg executable
+AudioSegment.converter = "C:\\Users\\yashr\\Downloads\\ffmpeg-7.1-full_build\\ffmpeg-7.1-full_build\\bin\\ffmpeg.exe"
+
+def speak(text):
+    tts = gtts.gTTS(text, lang='en')
+    tts.save("output.wav")
+
+    audio = AudioSegment.from_file("output.wav")
+    os.remove("output.wav")
+    audio = audio.speedup(playback_speed=1.5)
+
+    play(audio)
+
+def find_my_ip():
+    ip_address = requests.get('https://api64.ipify.org?format=json').json()
+    return ip_address["ip"]
+
+def search_on_wikipedia(query):
+    results = wikipedia.summary(query, sentences=2)
+    return results
+
+def search_on_google(query):
+    kit.search(query)
+
+def youtube(video):
+    kit.playonyt(video)
+
+def send_email(receiver_add, subject, message):
+    try:
+        email = EmailMessage()
+        email['To'] = receiver_add
+        email['Subject'] = subject
+        email['From'] = EMAIL
+
+        email.set_content(message)
+        s = smtplib.SMTP(SMTP_URL, SMTP_PORT)
+        s.starttls()
+        s.login(EMAIL, PASSWORD)
+        s.send_message(email)
+        s.close()
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+
+def get_news():
+    news_headline = []
+    result = requests.get(
+        NEWS_FETCH_API_URL,
+        params={
+            "country": "in",
+            "category": "general",
+            "apiKey": NEWS_FETCH_API_KEY
+        },
+    ).json()
+    articles = result["articles"]
+    for article in articles:
+        news_headline.append(article["title"])
+    return news_headline[:6]
+
